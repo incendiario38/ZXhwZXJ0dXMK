fb84c9c4832fdcfc7871a191e1fcdb36b5bc55b5
kravec <kravec2001@mail.ru>
2021-06-19 09:48:39 +0700

    Правка грида случаев лечения

--- src/Repository/ExpVrachRepository.php
+++ src/Repository/ExpVrachRepository.php
@@ -26,20 +26,54 @@ class ExpVrachRepository extends ServiceEntityRepository
     public function getExpSluch(ExpVrach $vrach): array
     {
         $sql = <<<SQL
-select
-t.id_pers, t.dbeg, t.dend, t.usl_ok, t.mkb, t.forma, t.phase, t.stage, t.id, t.n_med_card,
-       p.fam, p.im, p.ot, p.dr, p.mr,
-CASE
-    WHEN p.w = 1 then
-        'Мужской'
-    WHEN p.w = 0 then
-        'Женский'
-        else
-            'Не заполнен'
-    end as w,  p.enp
+select t.id_pers,
+       t.dbeg,
+       t.dend,
+       CASE
+           WHEN t.usl_ok = 0 THEN 'Стационар'
+           WHEN t.usl_ok = 4 THEN 'Поликлиника'
+           WHEN t.usl_ok = 9 THEN 'Скорая помощь'
+           else
+               'Не заполнен'
+           end as usl_ok,
+
+       t.mkb,
+       CASE
+           WHEN t.forma = 1 THEN 'Плановая'
+           WHEN t.forma = 1 THEN 'Экстренная'
+           WHEN t.forma = 1 THEN 'Неотложная'
+           else
+               'Не заполнен'
+           end AS forma,
+       CASE
+           WHEN t.phase = 1 THEN 'нет'
+           WHEN t.phase = 2 THEN 'Острая'
+           else 'нет'
+           end AS phase,
+       CASE
+           WHEN t.stage = 1 THEN 'нет'
+           WHEN t.stage = 2 THEN 'Тяжелая'
+           else 'нет'
+           end AS stage,
+       t.id,
+       t.n_med_card,
+       p.fam,
+       p.im,
+       p.ot,
+       p.dr,
+       p.mr,
+       CASE
+           WHEN p.w = 1 then
+               'Мужской'
+           WHEN p.w = 0 then
+               'Женский'
+           else
+               'Не заполнен'
+           end as w,
+       p.enp
 from exp_sluch t
-left join exp_patient p on p.id_pers = t.id_pers
-left join exp_lpu l on l.lpu_id = t.lpu_id
+         left join exp_patient p on p.id_pers = t.id_pers
+         left join exp_lpu l on l.lpu_id = t.lpu_id
 where t.id_vrach = :vrach 
 SQL;
 
@@ -86,7 +120,7 @@ SQL;
 
         try {
             return $query->getSingleScalarResult();
-        } catch (NoResultException|NonUniqueResultException $e) {
+        } catch (NoResultException | NonUniqueResultException $e) {
             return 0;
         }
     }
